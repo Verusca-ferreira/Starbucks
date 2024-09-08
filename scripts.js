b313765ce9b942291d4076c76f78f218f0eace83
Verusca-ferreira <ve_ferreiraa@hotmail.com>
2024-09-07 21:39:23 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector(".circulo")
+
+function trocaImagem(endereco) {
+
+    imagem.src = endereco
+
+}
+
+function trocaCor (cor) {
+
+circulo.style.background = cor
+
+}
